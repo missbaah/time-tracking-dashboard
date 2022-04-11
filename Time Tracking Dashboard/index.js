9f10b23d47423be1991c93048778ab0273e180a0
unknown <88596431+missbaah@users.noreply.github.com>
2022-04-11 17:41:01 +0000

    adding js

--- Time Tracking Dashboard/index.js
+++ Time Tracking Dashboard/index.js
@@ -1,9 +1,142 @@
-const daily = document.querySelector(".daily");
-const links = document.getElementsByTagName('a')
+let data = [
+    {
+      "title": "Work",
+      "timeframes": {
+        "daily": {
+          "current": 5,
+          "previous": 7
+        },
+        "weekly": {
+          "current": 32,
+          "previous": 36
+        },
+        "monthly": {
+          "current": 103,
+          "previous": 128
+        }
+      }
+    },
+    {
+      "title": "Play",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 2
+        },
+        "weekly": {
+          "current": 10,
+          "previous": 8
+        },
+        "monthly": {
+          "current": 23,
+          "previous": 29
+        }
+      }
+    },
+    {
+      "title": "Study",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 7
+        },
+        "monthly": {
+          "current": 13,
+          "previous": 19
+        }
+      }
+    },
+    {
+      "title": "Exercise",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 5
+        },
+        "monthly": {
+          "current": 11,
+          "previous": 18
+        }
+      }
+    },
+    {
+      "title": "Social",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 3
+        },
+        "weekly": {
+          "current": 5,
+          "previous": 10
+        },
+        "monthly": {
+          "current": 21,
+          "previous": 23
+        }
+      }
+    },
+    {
+      "title": "Self Care",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 2,
+          "previous": 2
+        },
+        "monthly": {
+          "current": 7,
+          "previous": 11
+        }
+      }
+    }
+  ]
 
-   links[0].addEventListener('click', function(){
-       if(daily.onclick){
-           document.querySelectorAll(".dailyhours") = document.querySelectorAll(".dailyhours").innerHTML
-           document.querySelectorAll(".yesterday") = document.querySelectorAll(".yesterday").innerHTML
-       }
-   })
+const nodelist =  document.querySelectorAll('periodical-option')
+const periodical = Array.apply(null, nodelist);
+
+
+const activateClickedLink = (periodical) => {
+    periodical.forEach(periodical.classList.remove('active'))
+    periodical.classList.add('active')
+}
+
+const renderCards = (clickedOption) => {
+    
+    const calcTimeframe = (option) => {
+        if (option === 'daily'){
+            return 'Yesterday'
+        } else if ( option === 'weekly'){
+            return 'Last Week'
+        } else if  ( option === 'monthly'){
+            return 'Last Month'
+        }
+    }
+    
+    data.forEach(activity => {
+        const name = activity.title
+        const activityClass = name.toLowerCase().replace(' ', '-')
+        const timeframeData = activity.timeframes[clickedOption]
+        const previousTimeframe = calcTimeframe(clickedOption)
+        console.log(previousTimeframe)
+    })
+}
+ 
+periodical.forEach ( periodical => {
+    periodical.addEventListener( 'click', () => {
+        activateClickedLink(periodical)
+        const clickedOption = periodical.dataset.option 
+        renderCards(clickedOption)
+    })
+})
